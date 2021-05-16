0ba65988a6a708b5123e8d6e9108520cedcd9479
Saimun <saimun4u@gmail.com>
2021-05-15 23:19:19 -0500

    Making initial setup for sending email Sending an email

--- script.py
+++ script.py
@@ -2,6 +2,8 @@ import smtplib
 
 conn = smtplib.SMTP('smtp.gmail.com', 587)
 
+# Port number for the smtp protocol is 587
+
 # print(type(conn))
 
 # Connecting to the server
@@ -11,3 +13,11 @@ print(conn.ehlo())
 # Ensuring that any password sent is encrypted
 
 print(conn.starttls())
+
+print(conn.login('saimun4u@gmail.com', 'xmtp_su8'))
+
+#Password is imaginary in this case.
+
+conn.sendmail('saimun4u@gmail.com', 'saimun4u@gmail.com', 'Subject: So long...\n\nDear Saimun\n, \nHow is it going?\n\n - From Sophie')
+
+conn.quit()
